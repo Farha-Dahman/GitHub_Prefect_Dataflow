bee991837998c5f7c18f512119b908ec39f87fad
Farha Dahman <farhajehad@gmail.com>
2024-05-05 01:07:30 +0300

    modify connection to the DB

--- database/connection.py
+++ database/connection.py
@@ -6,11 +6,16 @@ def connection_db(db_name):
     """
     Function to establish connection to MongoDB using the connection string from environment variables.
     """
-    # Load environment variables from .env file
-    load_dotenv()
+    try:
+        # Load environment variables from .env file
+        load_dotenv()
 
-    # Get the MongoDB connection string from environment variables
-    uri = os.getenv("CONNECTION_STRING")
+        # Get the MongoDB connection string from environment variables
+        uri = os.getenv("CONNECTION_STRING")
 
-    # Establish the mongoengine connection
-    connect(host=uri, db=db_name)
+        # Establish the mongoengine connection
+        connect(host=uri, db=db_name)
+
+        return f"Connected to MongoDB: {uri}"
+    except Exception as e:
+        raise ValueError(f"Error connecting to MongoDB: {e}")
